9c4f4e706c2c9bc4a9227cbe07622f0b9593fabd
Edson <edson_junior2002@hotmail.com>
2025-11-20 11:19:09 -0300

    Adiciona testes para validaCartao

--- src/test/java/com/scb/externo/service/ExternoServiceTest.java
+++ src/test/java/com/scb/externo/service/ExternoServiceTest.java
@@ -3,6 +3,7 @@ package com.scb.externo.service;
 import com.scb.externo.dto.Cobranca;
 import com.scb.externo.dto.Email;
 import com.scb.externo.dto.NovaCobranca;
+import com.scb.externo.dto.NovoCartaoDeCredito;
 import com.scb.externo.dto.NovoEmail;
 import com.scb.externo.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
@@ -87,18 +88,18 @@ class ExternoServiceTest {
     }
 
     @Test
-    void valiaNumero_deveRetornarTrueParaNumeroValido() {
+    void validaNumero_deveRetornarTrueParaNumeroValido() {
         // número válido pelo algoritmo de Luhn
         assertTrue(service.validaNumero("79927398713"));
     }
 
     @Test
-    void valiaNumero_deveRetornarFalseParaNumeroInvalido() {
+    void validaNumero_deveRetornarFalseParaNumeroInvalido() {
         assertFalse(service.validaNumero("1234567890"));
     }
 
     @Test
-    void valiaNumero_deveRetornarFalseParaCaracterInvalido() {
+    void validaNumero_deveRetornarFalseParaCaracterInvalido() {
         assertFalse(service.validaNumero("abcd"));
     }
 
@@ -127,4 +128,18 @@ class ExternoServiceTest {
                 assertTrue(Set.of("PAGA", "FALHA").contains(c.status()))
         );
     }
+    @Test
+    void validaCartao_deveRetornarTrue() {
+        // ajuste os outros parâmetros, se o seu NovoCartaoDeCredito tiver mais campos
+        NovoCartaoDeCredito cartao = new NovoCartaoDeCredito("79927398713", "Edson", "29/11","132");
+        assertTrue(service.validaCartao(cartao));
+    }
+
+    @Test
+    void validaCartao_deveRetornarFalse() {
+        // ajuste os outros parâmetros, se o seu NovoCartaoDeCredito tiver mais campos
+        NovoCartaoDeCredito cartao = new NovoCartaoDeCredito("1234567890", "Raul", "18/07", "311");
+        assertFalse(service.validaCartao(cartao));
+    }
+
 }
