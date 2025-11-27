fccb1baf1e772b7b6d917dfd4402514c113129b6
Edson <edson_junior2002@hotmail.com>
2025-11-26 22:01:01 -0300

    Adicionando mais testes a classe ExternoController e Substituindo testes na classe MailgunGat

--- src/test/java/com/scb/externo/controller/ExternoControllerTest.java
+++ src/test/java/com/scb/externo/controller/ExternoControllerTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -23,34 +24,6 @@ class ExternoControllerTest {
     @Mock
     private ExternoService service;
 
-    /*@Test
-    void validaCartaoDeCredito_deveRetornar200QuandoValido() {
-        NovoCartaoDeCredito dto =
-                new NovoCartaoDeCredito("4539578763621486","Fulana","12/28","123");
-
-        when(service.validaCartao(dto)).thenReturn(true);
-
-        ResponseEntity<String> resp = controller.validaCartao(dto);
-
-        assertEquals(HttpStatus.OK, resp.getStatusCode());
-        assertEquals("Dados atualizados", resp.getBody());
-        verify(service).validaCartao(dto);
-    }
-
-    @Test
-    void validaCartaoDeCredito_deveRetornar422QuandoInvalido() {
-        NovoCartaoDeCredito dto =
-                new NovoCartaoDeCredito("1111111111111111","Fulana","12/28","123");
-
-        when(service.validaCartao(dto)).thenReturn(false);
-
-        ResponseEntity<String> resp = controller.validaCartao(dto);
-
-        assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, resp.getStatusCode());
-        assertTrue(resp.getBody().startsWith("422:"));
-        verify(service).validaCartao(dto);
-    }
-
     @Test
     void enviarEmail_deveRetornar200QuandoServiceOK() {
         NovoEmail req = new NovoEmail("fulana@ex.com","Cadastro realizado");
@@ -63,7 +36,7 @@ class ExternoControllerTest {
         assertEquals(HttpStatus.OK, resp.getStatusCode());
         assertEquals(email, resp.getBody());
         verify(service).enviarEmail(req);
-    }*/
+    }
 
     @Test
     void enviarEmail_devePropagarNotFound() {
@@ -84,58 +57,24 @@ class ExternoControllerTest {
         verify(service).restaurarBanco();
     }
 
-    /*@Test
-    void postCobranca_deveRetornarCobranca() {
-        NovaCobranca req = new NovaCobranca("123", 1990L);
-        Cobranca c = new Cobranca(1L, "123", 1990L, "SOLICITADA","gateway-fake-0");
-
-        when(service.criarCobranca(req)).thenReturn(c);
-
-        ResponseEntity<Cobranca> resp = controller.postCobranca(req);
-
-        assertEquals(HttpStatus.OK, resp.getStatusCode());
-        assertEquals(c, resp.getBody());
-        verify(service).criarCobranca(req);
-    }
-
     @Test
-    void filaCobranca_deveRetornarCobrancaEmFila() {
-        NovaCobranca req = new NovaCobranca("123", 2990L);
-        Cobranca c = new Cobranca(2L, "123", 2990L, "EM_FILA", "gateway-fake-1");
+    void pagarCobranca_deveChamarServicoEDevolverCobranca() {
+        Cobranca resp = new Cobranca(
+                30L,
+                "PAGA",
+                Instant.now(),
+                Instant.now(),
+                300L,
+                "ciclistaPago",
+                "pi_pago"
+        );
 
-        when(service.incluirNaFila(req)).thenReturn(c);
+        when(service.pagarCobranca(30L)).thenReturn(resp);
 
-        ResponseEntity<Cobranca> resp = controller.filaCobranca(req);
+        var resultado = controller.pagarCobranca(30L);
 
-        assertEquals(HttpStatus.OK, resp.getStatusCode());
-        assertEquals(c, resp.getBody());
-        verify(service).incluirNaFila(req);
+        assertEquals(200, resultado.getStatusCode().value());
+        assertEquals("PAGA", resultado.getBody().status());
+        verify(service).pagarCobranca(30L);
     }
-
-    @Test
-    void getCobranca_deveRetornarCobranca() {
-        Cobranca c = new Cobranca(99L, "123", 10L, "PAGA", "gateway-fake-2");
-        when(service.obterCobranca(99L)).thenReturn(c);
-
-        ResponseEntity<Cobranca> resp = controller.getCobranca(99L);
-
-        assertEquals(HttpStatus.OK, resp.getStatusCode());
-        assertEquals(c, resp.getBody());
-        verify(service).obterCobranca(99L);
-    }
-
-    @Test
-    void processaCobrancasEmFila_deveRetornarLista() {
-        Cobranca c1 = new Cobranca(1L, "123", 10L, "PAGA","gateway-fake-3");
-        Cobranca c2 = new Cobranca(2L, "456", 20L, "FALHA", "gateway-fake-4");
-
-        when(service.processarFila()).thenReturn(List.of(c1, c2));
-
-        ResponseEntity<List<Cobranca>> resp = controller.processaCobrancasEmFila();
-
-        assertEquals(HttpStatus.OK, resp.getStatusCode());
-        assertEquals(List.of(c1, c2), resp.getBody());
-        verify(service).processarFila();
-    }*/
-
 }
